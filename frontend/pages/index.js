1abec40361aa1659465e1d044c08fcbb761abc06
Michał Junke <michal.junke@gmail.com>
2020-04-14 15:05:07 +0200

    refactor: Create base for developing index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
+
 import WPAPI from 'wpapi';
 import Layout from '../components/Layout';
 import PageWrapper from '../components/PageWrapper';
 import Menu from '../components/Menu/Menu';
 import Config from '../config';
 
+import Re from '../public/images/re-home-page.svg';
 const wp = new WPAPI({ endpoint: Config.apiUrl });
 
 const headerImageStyle = {
@@ -29,19 +31,16 @@ class Index extends Component {
 
   static async getInitialProps() {
     try {
-      const [page, posts, pages] = await Promise.all([
+      const page = await
         wp
           .pages()
-          .slug('welcome')
+          .slug('strona-glowna')
           .embed()
           .then(data => {
             return data[0];
-          }),
-        wp.posts().embed(),
-        wp.pages().embed(),
-      ]);
+          });
 
-      return { page, posts, pages };
+      return { page };
     } catch (err) {
       if (err.data.status === 403) {
         tokenExpired();
@@ -51,7 +50,7 @@ class Index extends Component {
     return null;
   }
 
-  componentDidMount() {
+/*   componentDidMount() {
     const token = localStorage.getItem(Config.AUTH_TOKEN);
     if (token) {
       wp.setHeaders('Authorization', `Bearer ${token}`);
@@ -67,79 +66,25 @@ class Index extends Component {
           }
         });
     }
-  }
+  } */
 
   render() {
     const { id } = this.state;
-    const { posts, pages, headerMenu, page } = this.props;
-    const fposts = posts.map(post => {
-      return (
-        <ul key={post.slug}>
-          <li>
-            <Link
-              as={`/post/${post.slug}`}
-              href={`/post?slug=${post.slug}&apiRoute=post`}
-            >
-              <a>{post.title.rendered}</a>
-            </Link>
-          </li>
-        </ul>
-      );
-    });
-    const fpages = pages.map(ipage => {
-      if (ipage.slug !== 'welcome') {
-        return (
-          <ul key={ipage.slug}>
-            <li>
-              <Link
-                as={`/page/${ipage.slug}`}
-                href={`/post?slug=${ipage.slug}&apiRoute=page`}
-              >
-                <a>{ipage.title.rendered}</a>
-              </Link>
-            </li>
-          </ul>
-        );
-      }
-    });
+    const { headerMenu, page } = this.props;
 
     return (
       <Layout>
-        <Menu menu={headerMenu} />
-        <div className="intro bg-black white ph3 pv4 ph5-m pv5-l flex flex-column flex-row-l">
-          <div className="color-logo w-50-l mr3-l">
-            <Logo width={440} height={280} />
-          </div>
-          <div className="subhed pr6-l">
-            <h1>{page.title.rendered}</h1>
-            <div className="dek">
-              You are now running a WordPress backend with a React frontend.
-            </div>
-            <div className="api-info b mt4">
-              Starter Kit supports both REST API and GraphQL
-              <div className="api-toggle">
-                <a className="rest" href="http://localhost:3000">REST API</a>
-                <a className="graphql" href="http://localhost:3001">GraphQL</a>
-              </div>
-            </div>
-          </div>
+        <Menu menu={headerMenu} padding/>
+        <div className="relative"> 
+        <img src="https://via.placeholder.com/600x700" alt="" className="w-full my-4"/>
+        <Re className={`absolute bottom-5 right-5`}/>
         </div>
-        <div className="recent flex mh4 mv4 w-two-thirds-l center-l">
-          <div className="w-50 pr3">
-            <h2>Posts</h2>
-            {fposts}
-          </div>
-          <div className="w-50 pl3">
-            <h2>Pages</h2>
-            {fpages}
-          </div>
-        </div>
-        <div className="content mh4 mv4 w-two-thirds-l center-l home"
+        {/* <div
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html: page.content.rendered,
           }}
-        />
+        /> */}
       </Layout>
     );
   }
