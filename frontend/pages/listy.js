80a84d328c37b4ad35050791bfdac978f9e4bf4a
Michał Junke <michal.junke@gmail.com>
2020-04-14 15:14:15 +0200

    Feat: Add listy page
    Just a placeholder for further development

---
+++ frontend/pages/listy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Listy = () => (
+    <>
+    <p>To tylko testowa strona :)</p>
+    <Link href="/"><a>Strona główna</a></Link>
+    </>
+)
+
+export default Listy
