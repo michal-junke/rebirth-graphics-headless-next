35b716cd41975a69c04107e55c918ecce7f11ebe
Michał Junke <michal.junke@gmail.com>
2020-04-15 15:52:06 +0200

    feat: Add css module styles for index
    Also adding new font for index styles.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,6 +8,7 @@ import PageWrapper from '../components/PageWrapper';
 import Menu from '../components/Menu/Menu';
 
 import Config from '../config';
+import styles from './styles/index.module.css';
 
 import Re from '../public/images/re-home-page.svg';
 const wp = new WPAPI({ endpoint: Config.apiUrl });
@@ -28,7 +29,8 @@ const tokenExpired = () => {
 
 class Index extends Component {
 
-  static async getInitialProps() { 
+  static async getInitialProps() {
+    
     try {
       const page = await
         wp
@@ -44,9 +46,12 @@ class Index extends Component {
           .globalOptions()
           .then(data => data.acf);
 
+
       return { page, acfOptions };
     } catch (err) {
+      
       if (err.data.status === 403) {
+        
         tokenExpired();
       }
     }
@@ -61,7 +66,7 @@ class Index extends Component {
         <Menu menu={headerMenu} padding/>
         <div className="relative"> 
         <img src="https://via.placeholder.com/600x700" alt="" className="w-full my-4"/>
-        <Re className={`absolute bottom-5 right-5`}/>
+        <Re className={[styles['bottom-5'], styles['right-5'], 'absolute'].join(' ')}/>
         </div>
         <section className="social">
           <p className={styles.tilted}>... passion for creation ...</p>
