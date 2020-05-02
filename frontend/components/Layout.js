70e17d9febbf30d60385abbee664b85acb3d744c
Michał Junke <michal.junke@gmail.com>
2020-05-02 16:08:15 +0200

    fix: Remove padding

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -3,14 +3,14 @@ import Header from './Header';
 import Footer from './Footer/Footer';
 
 const Layout = props => {
-  const { children, acfOptions } = props;
+  const { children, acfOptions, padding } = props;
   return (
     <div className="pt-8 md:pt-0">
       <Header />
-      <main className={`py-10 ${props.padding ? 'px-3 md:px-24' : ''}`}>
+      <main className={`${padding ? 'px-3 md:px-24' : ''}`}>
         {children}
       </main>
-      <Footer acfOptions={acfOptions}/>
+      <Footer acfOptions={acfOptions} />
     </div>
   );
 };
