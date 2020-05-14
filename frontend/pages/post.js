3ed800c94e2acb17a2ea9feeeb13f1ee9cd9d5aa
Michał Junke <michal.junke@gmail.com>
2020-05-14 08:16:22 +0200

    feat(post): Add new post layout

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -43,28 +43,36 @@ class Post extends Component {
       );
     }
 
-    const heroUrl = (
-      post._embedded
-      && post._embedded['wp:featuredmedia']
-      && post._embedded['wp:featuredmedia'][0]
-      && post._embedded['wp:featuredmedia'][0].source_url
-    ) ? post._embedded['wp:featuredmedia'][0].source_url : false;
-
+    const heroUrl = (post.acf.featured_gallery[0]);
+    const tags = (post.tag_names.map(tag => (
+      <span key={tag} className="pr-1">
+        #
+        {tag}
+      </span>
+    )));
     return (
-      <Layout className="test" acfOptions={acfOptions}>
-        <Menu menu={headerMenu} />
+      <Layout className="md:pt-32 lg:pt-56" acfOptions={acfOptions}>
+        <Menu menu={headerMenu} isFixed />
+        <div className="grid-margin">
+          <p className="text-right mt-2 pb-4 md:pb-6 md:mx-auto" style={{ maxWidth: '674px' }}>{post.formatted_date}</p>
+          {/* eslint-disable-next-line react/no-danger */}
+          <h1 style={{ maxWidth: '33rem' }} className="text-center mx-auto block" dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <hr className="hidden md:block mx-auto my-4" style={{ maxWidth: '674px', borderColor: '#000', borderTopWidth: '0.5px' }} />
+          <div className="tags pb-3 text-center hidden md:block">
+            {tags}
+          </div>
+        </div>
         {heroUrl ? (
           <div className={`hero flex items-center post-type-${post.type}`}>
-            <img
-              className="w-100"
-              src={heroUrl}
-              alt=""
-            />
+            <img className="mx-auto block py-4" src={heroUrl} alt="" />
           </div>
         ) : ''}
-        <div className={`content mh4 mv4 w-two-thirds-l center-l post-${post.id} post-type-${post.type}`}>
-          <h1>{post.title.rendered}</h1>
+        <div className="tags text-center md:hidden">
+          {tags}
+        </div>
+        <div className={`post-content post-${post.id} post-type-${post.type}${post.categories[0] === 3 ? ` ${styles.letter}` : '' }`}>
           <div
+            className="block mx-auto"
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html: post.content.rendered,
