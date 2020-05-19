c2fb4788bc714aa7fe723f06e89738c08ab538f1
Michał Junke <michal.junke@gmail.com>
2020-05-19 06:26:49 +0200

    style(GlideSlider): Add eslint fixes

--- frontend/components/GlideSlider/GlideSlider.js
+++ frontend/components/GlideSlider/GlideSlider.js
@@ -10,55 +10,59 @@ class GlideSlider extends Component {
     }
 
     initializeSlider = () => {
+      const {
+        options, instance, mobile, desktop,
+      } = this.props;
+      const initialize = () => {
+        const glide = document.querySelector(`.glide-${instance}`);
+        this.glide = new Glide(glide, options);
+        this.glide.mount();
+      };
 
-        let initialize = () => {
-            let glide = document.querySelector(`.glide-${this.props.instance}`);
-            this.glide = new Glide (glide, this.props.options);
-            this.glide.mount();
+      if (mobile) {
+        if (window.innerWidth < 768) {
+          initialize();
         }
-
-        if (this.props.mobile) {
-            if (window.innerWidth < 768) {
-                initialize();
-            }
-        } else if (this.props.desktop) {
-            if (window.innerWidth >= 768) {    
-            initialize();
-            }
-        } else {
-            initialize();
+      } else if (desktop) {
+        if (window.innerWidth >= 768) {
+          initialize();
         }
+      } else {
+        initialize();
+      }
     }
-        
+
 
     componentWillUnmount = () => {
-        if (this.glide) {
-            this.glide.destroy();
-        }
+      if (this.glide) {
+        this.glide.destroy();
+      }
     }
-    
-    
-   render() { 
-       return (
-           <>
-    <div className="overflow-hidden w-full">
-    <div id="slider" className={`glide-${this.props.instance} ${this.props.className}`}>
-        <div className="glide__track" data-glide-el="track">
-        <ul className="glide__slides flex">
-            {this.props.children.map( (slide, index) => {
-                return(
-                <li className="glide__slide" key={index}>
-                    {slide}
-                </li>
-                )
-            })}
-        </ul>
-        </div>
-    </div>
-    </div>
-    </>
-)
-   }
+
+
+    render() {
+      const {
+        instance, className, children,
+      } = this.props;
+      return (
+        <>
+          <div className="overflow-hidden w-full">
+            <div id="slider" className={`glide-${instance} ${className}`}>
+              <div className="glide__track" data-glide-el="track">
+                <ul className="glide__slides flex">
+                  {children.map((slide, index) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <li className="glide__slide" key={index}>
+                      {slide}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </>
+      );
     }
+}
 
 export default GlideSlider;
