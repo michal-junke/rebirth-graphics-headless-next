42b1da019d2ac337e60e5b703e5942478eb1ed02
Michał Junke <michal.junke@gmail.com>
2020-05-20 22:06:05 +0200

    feat(SeeAlso): New layout

--- frontend/components/SeeAlso/SeeAlso.js
+++ frontend/components/SeeAlso/SeeAlso.js
@@ -1,18 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Link from 'next/link';
 import GlideSlider from '../GlideSlider/GlideSlider';
 
+import styles from './SeeAlso.module.scss';
+
 const SeeAlso = props => {
-  const { category } = props;
+  const { category, buttons } = props;
   return (
     <>
-      <h2 className="text-2xl text-center">poznaj też:</h2>
-      <h3 className="font-sans uppercase grid-padding pb-1">
+      <h2 className="text-3xl text-center">poznaj też:</h2>
+      <h3 className="font-sans uppercase grid-padding pb-1 text-4xl">
         {category.details.acf.interpunction}
         {category.details.name}
       </h3>
       <GlideSlider
         instance="category"
-        className=""
+        className=" mb-10 md:mb-32"
+        buttons={buttons}
         options={{
           type: 'slider',
           peek: { before: 0, after: 100 },
@@ -33,13 +38,20 @@ const SeeAlso = props => {
         }}
       >
         {category.posts.map(post => (
-          <div key={post.id}>
-            <img src={post.acf.featured_gallery[0]} alt="" />
-            {/* eslint-disable-next-line react/no-danger */}
-            <h4 className="pt-1 pb-4 text-lg" dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <div key={post.id} className={`${styles.seeAlso} relative`}>
+            <Link href={`/${post.cat_slug}/${post.slug}`}>
+              <a>
+                <img src={post.acf.featured_gallery[0]} alt="" />
+                <div className={`${styles.apla} md:absolute md:inset-0 md:invisible`}>
+                  {/* eslint-disable-next-line react/no-danger */}
+                  <h4 className="pt-1 pb-4 text-xl leading-tight" dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </GlideSlider>
+      <hr className={`mb-6 ${styles.hr}`} />
     </>
   );
 };
