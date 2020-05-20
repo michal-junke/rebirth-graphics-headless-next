7a1a3d19207799e735e4ced572b39a1d01097a0f
Michał Junke <michal.junke@gmail.com>
2020-05-20 22:12:33 +0200

    fix(SeeAlso): Show prev and next buttons

--- frontend/components/GlideSlider/GlideSlider.js
+++ frontend/components/GlideSlider/GlideSlider.js
@@ -49,12 +49,12 @@ class GlideSlider extends Component {
           <div className="overflow-hidden w-full">
             <div id="slider" className={`glide-${instance} ${className} relative`}>
               <div className="absolute w-full h-full" data-glide-el="controls">
-                <button type="button" className={`${buttons ? 'md:absolute' : ''} hidden right-0 inset-y-0 z-10 pr-6 text-white`} data-glide-dir=">">
+                <button type="button" className={`${buttons ? 'md:block' : ''} hidden absolute right-0 inset-y-0 z-10 pr-6 text-white`} data-glide-dir=">">
                   <svg width="61" height="61" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M30.5 5.33279e-06C13.664 2.38909e-06 -2.38909e-06 13.664 -5.33279e-06 30.5C-8.27649e-06 47.336 13.664 61 30.5 61C47.336 61 61 47.336 61 30.5C61 13.664 47.336 8.27649e-06 30.5 5.33279e-06ZM24.4 44.225L24.4 16.775L42.7 30.5L24.4 44.225Z" fill="white" fillOpacity="0.7" />
                   </svg>
                 </button>
-                <button type="button" className={`${buttons ? 'md:absolute' : ''} hidden left-0 inset-y-0 z-10 pl-6 text-white`} data-glide-dir="<">
+                <button type="button" className={`${buttons ? 'md:block' : ''} hidden absolute left-0 inset-y-0 z-10 pl-6 text-white`} data-glide-dir="<">
                   <svg width="61" height="61" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M30.5 61C47.336 61 61 47.336 61 30.5C61 13.664 47.336 -2.62015e-06 30.5 -1.1483e-06C13.664 3.23547e-07 3.49115e-06 13.664 4.963e-06 30.5C6.43485e-06 47.336 13.664 61 30.5 61ZM36.6 16.775L36.6 44.225L18.3 30.5L36.6 16.775Z" fill="white" fillOpacity="0.7" />
                   </svg>
